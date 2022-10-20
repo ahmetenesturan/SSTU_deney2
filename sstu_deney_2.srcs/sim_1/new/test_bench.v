61e8b109d2b167e51fde116b99113a75ffc55b80
Ahmet Enes TURAN <ahmetenesturan@gmail.com>
2022-10-20 14:34:04 +0300

    source files added

---
+++ sstu_deney_2.srcs/sim_1/new/test_bench.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.10.2022 14:32:09
+// Design Name: 
+// Module Name: test_bench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_bench(
+
+    );
+endmodule
